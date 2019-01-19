475b699f2fd1122d7452f84ff98c78815f784099
Jonathan Daly <dalyj138@gmail.com>
2019-01-18 21:28:41 -0500

    Finished MVP.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,30 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+let c = 1738;
+function timesPI(x) {
+  x = (Math.PI * x);
+  return function(n) {
+    return (n * x);
+  }
+}
+const multiplyPI = timesPI(50);
+let xPI = multiplyPI(c);
+console.log(xPI.toFixed(9));
 
 // ==== Challenge 2: Create a counter function ====
+
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function() {
+    return ++count;
+  }
 };
+const newCounter = counter();
+//console.log(newCounter);
+console.log(newCounter());
+console.log(newCounter());
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
